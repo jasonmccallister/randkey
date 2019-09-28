e25e20c5e6a4a8f18293f0329aee821c34727372
Jason McCallister <themccallister@gmail.com>
2019-09-28 18:52:07 -0400

    add query param

--- api/key.go
+++ api/key.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/jasonmccallister/randkey/pkg/key"
 )
@@ -13,8 +14,10 @@ type response struct {
 
 // Handler is the endpoint that creates the serverless function for Zeit
 func Handler(w http.ResponseWriter, r *http.Request) {
+	l := getLength(r)
+
 	resp := response{
-		Key: key.Generate(8),
+		Key: key.Generate(l),
 	}
 
 	body, err := json.Marshal(resp)
@@ -25,4 +28,19 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
+}
+
+func getLength(r *http.Request) int {
+	param := r.URL.Query().Get("length")
+
+	if param != "" {
+		i, err := strconv.Atoi(param)
+		if err != nil {
+			return 8
+		}
+
+		return i
+	}
+
+	return 8
 }
